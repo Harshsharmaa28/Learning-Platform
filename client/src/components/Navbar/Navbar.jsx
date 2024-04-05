f5d879ba9a38f74a6480e050d5602d791f346b0a
Harsh Sharma <harshsharma0828@gmail.com>
2024-04-05 15:33:52 +0530

    added navbar & Landingpage

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
+import { ArrowRight } from 'lucide-react'
 
 const Navbar = () => {
   return (
-    <div>
-      <div>
-        <div></div>
-        <div></div>
-        <div></div>
+    <div className=' flex justify-center gap-20 py-4'>
+      <div className=' flex gap-20 items-center'>
+        <div className=' font-medium hover:cursor-pointer'>Home</div>
+        <div className=' font-medium hover:cursor-pointer'>Academics</div>
+        <div className=' font-medium hover:cursor-pointer'>Mentors</div>
       </div>
-      <img className=' bg-slate-500 w-[10rem] h-[10rem]' src="./assests/icon.png" alt="error" />
-      <div>
-        <div></div>
-        <div></div>
-        <div></div>
+      <img className=' hover:cursor-pointer w-[12rem] h-[3.5rem]' src="./assests/Bicon.png" alt="error" />
+      <div className=' items-center flex gap-20'>
+        <div className=' font-medium hover:cursor-pointer'>About Us</div>
+        <div className=' font-medium hover:cursor-pointer'>Contact Us</div>
+        <button
+          type="button"
+          className=" flex rounded-md bg-black px-8 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+        >
+          Register
+          <ArrowRight className=" mt-1 ml-2 h-4 w-4" />
+        </button>
       </div>
     </div>
   )
